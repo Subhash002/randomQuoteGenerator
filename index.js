df121fd50ede06a2ce06d3a7ac11e6bb28eae6ee
Subhash Kandhway <kandhway002subhash@gmail.com>
2023-08-02 12:49:57 +0530

    quotemaster

---
+++ index.js
@@ -0,0 +1,52 @@
+const quoteText = document.querySelector(".quote"),
+  quoteBtn = document.querySelector("button"),
+  authorName = document.querySelector(".name"),
+  speechBtn = document.querySelector(".speech"),
+  copyBtn = document.querySelector(".copy"),
+  twitterBtn = document.querySelector(".twitter"),
+  synth = speechSynthesis;
+
+function randomQuote() {
+  quoteBtn.classList.add("loading");
+  quoteBtn.innerText = "new Quote for you..";
+  fetch("https://www.jcquotes.com/api/quotes/random")
+    .then((data) => data.json())
+    .then((data) => data.text)
+    .then((data) => {
+      let i = 0;
+      quoteText.innerHTML = "";
+      const intervalId = setInterval(() => {
+        quoteText.innerHTML += data[i];
+        i++;
+        if (i === data.length) clearInterval(intervalId);
+      }, 50);
+      authorName.innerHTML = "James Clear";
+      quoteBtn.classList.remove("loading");
+      quoteBtn.innerText = "New Quote";
+    });
+}
+speechBtn.addEventListener("click", () => {
+  if (!quoteBtn.classList.contains("loading")) {
+    let utterance = new SpeechSynthesisUtterance(
+      `${quoteText.innerText} by ${authorName.innerText}`
+    );
+    synth.speak(utterance);
+    setInterval(() => {
+      !synth.speaking
+        ? speechBtn.classList.remove("active")
+        : speechBtn.classList.add("active");
+    }, 10);
+  }
+});
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(quoteText.innerText);
+});
+
+twitterBtn.addEventListener("click", () => {
+  let tweetURL = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+  window.open(tweetURL, "_blank");
+});
+
+quoteBtn.addEventListener("click", randomQuote);
+
+setInterval(randomQuote, 100000);
